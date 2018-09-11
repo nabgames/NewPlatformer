1791ae900428dcd922f70b7087dd35ae6d170b93
NickyTurk <41872855+NickyTurk@users.noreply.github.com>
2018-09-11 17:27:02 -0400

    Health&Menu
    -Added rudimentary health bar.
    -Added level select.
    -Changed player fire rate
    PS: not all of these files were edited

---
+++ MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+    public int select;
+    public int specific;
+    public int rangeMin = 1;
+    public int rangeMax = 5;
+    public string level;
+
+	// Use this for initialization
+	void Start () {
+        select = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.S)){
+            select++;
+        }
+        if(Input.GetKeyDown(KeyCode.W)){
+            select--;
+        }
+        if (select==specific){
+            GetComponent<TextMesh>().color = new Color(1.0f, 0.0f, 0.2f);
+        }
+        else{
+            GetComponent<TextMesh>().color = new Color(1.0f, 1.0f, 1.0f);
+        }
+        if (select<rangeMin){
+            select = rangeMax; 
+        }
+        if (select > rangeMax)
+        {
+            select = rangeMin;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print(specific);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+        }
+        }
+	}
+
