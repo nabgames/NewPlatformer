b199970f4ffbb3ee24f1c71b2491863f915938ce
NickyTurk <turknicky@gmail.com>
2019-02-11 18:58:04 -0500

    Revert "Reverting to "new stuff?""
    This reverts commit fd0eef70d8b42746ab72aa25365e3fba52fa5eb9.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -21,6 +21,7 @@ public class EnemyScript : MonoBehaviour {
 
     //Don't try to change these variables please! If you want to change specifications beyond the types above, maybe make chagnes to the serperate functions for movement below.
 
+    public bool moving;
     public float xMin;
     public float xMax;
     public float yMin;
@@ -32,6 +33,7 @@ public class EnemyScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        moving = false;
         Attack = 1f;
         moveRight = false;
         startX = transform.position.x;
@@ -308,7 +310,7 @@ public class EnemyScript : MonoBehaviour {
                 Attack = 0f;
             }
         }
-        if (Temper == "neutral")
+        if (Temper == "Neutral")
         {
             Attack = 0f;
         }
@@ -341,59 +343,35 @@ public class EnemyScript : MonoBehaviour {
                 Attack = 2f;
             }
         }
-        if (transform.position.x < xMin + Speed)
+       if (Attack != 0f)
+        // && Random.Range(0f, 3f) > 2f
         {
-            transform.position = new Vector3(transform.position.x + Speed, transform.position.y, 0.0f);
-        }
-        else if (transform.position.x > xMax - Speed)
-        {
-            transform.position = new Vector3(transform.position.x - Speed, transform.position.y, 0.0f);
-        }
-        else if (transform.position.y < yMin + Speed)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, 0.0f);
-        }
-        else if (transform.position.y > yMax - Speed)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, 0.0f);
-        }
-        else if (Attack != 0f && Random.Range(0f, 3f) > 2f)
-            
-        {
-            if (transform.position.x > playerScript.playerX)
+            if (playerScript.playerX < xMax && playerScript.playerY < yMax && playerScript.playerX > xMin && playerScript.playerY > yMin && moving == false)
             {
-                transform.position = new Vector3(transform.position.x - Speed * (Attack * 1.5f), transform.position.y, 0.0f);
-            }
-            else
-            {
-                transform.position = new Vector3(transform.position.x + Speed * (Attack * 1.5f), transform.position.y, 0.0f);
-            }
-            if (transform.position.y > playerScript.playerY)
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y - Speed * (Attack * 1.5f), 0.0f);
-            }
-            else
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y + Speed * (Attack * 1.5f), 0.0f);
+                GoTo(transform.position.x, transform.position.y, playerScript.playerX, playerScript.playerY, Speed * Attack);
             }
         }
-        else if (Random.Range(0 + moveBiasX, 4f) > 2f)
+        else if (moving == false)
         {
-            transform.position = new Vector3(transform.position.x - Speed, transform.position.y, 0.0f);
-        }
-        else
-        {
-            transform.position = new Vector3(transform.position.x + Speed, transform.position.y, 0.0f);
-        }
-        if (Random.Range(0 + moveBiasY, 4f) > 2f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, 0.0f);
-        }
-        else
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, 0.0f);
+            GoTo(transform.position.x, transform.position.y, Random.Range(xMin, xMax), Random.Range(xMin, xMax), Speed);
+            print("whale");
         }
 
+    }
+    void GoTo(float xS, float yS, float x, float y, float speed)
+    {
+        if (transform.position.x < x - .1f && transform.position.x > x + .1f && transform.position.y < y + .1f && transform.position.y > y - .1f)
+        {
+            moving = false;
+        }
+        else
+        {
+            moving = true;
+            transform.position = transform.position + new Vector3((x - xS) / (50f - speed), (y - yS) / (50f - speed), 0.0f);
+            
+        }
+
+            
     }
 }
 
